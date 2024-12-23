3b2c8ba86f242d77b08f5093ab3ae1df327c3cc6
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-12-23 11:17:00 +0900

    Replaced readBigUInt64BE with randomInt

--- src/audit.ts
+++ src/audit.ts
@@ -1,5 +1,6 @@
 import {randomBytes} from "node:crypto";
 import {MathNumericType, max, mean, min, std} from "mathjs";
+import {randomInt} from "node:crypto";
 
 // Supplied by Awesome Stickz on 2024-12-23
 function getWeightedCryptoRandomElement<T>(elements: { value: T; weight: number }[]) {
@@ -8,11 +9,12 @@ function getWeightedCryptoRandomElement<T>(elements: { value: T; weight: number 
     const totalWeight = elements.reduce((acc, element) => acc + element.weight, 0);
 
     // Generate 8 random bytes (64 bits)
-    const generatedRandomBytes = randomBytes(8);
+    // const generatedRandomBytes = randomBytes(8);
     // Convert the random bytes to an unsigned big-endian number
-    const randomBENumber = generatedRandomBytes.readBigUInt64BE(0);
+    // const randomBENumber = generatedRandomBytes.readBigUInt64BE(0);
 
-    const randomWeightedNumber = Number(randomBENumber % BigInt(totalWeight));
+    // const randomWeightedNumber = Number(randomBENumber % BigInt(totalWeight));
+    const randomWeightedNumber = randomInt(totalWeight);
 
     let cumulativeWeight = 0;
 
@@ -72,5 +74,5 @@ function analyseDrawings(n: number, drawings: number): void {
 
 for (let i = 2; i <= 1000; i = Math.max(i + 1, i * 1.2)) {
     const p = Math.floor(i);
-    analyseDrawings(p, 1e6);
+    analyseDrawings(p, p * 100e3);
 }
